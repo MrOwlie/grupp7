afa1b23c89fd6917d39bc6fb72cacb1d6985dbfa
MrOwlie <fredrik.uvgard@gmail.com>
2017-02-06 18:19:31 +0100

    added thirdParty db functions.

--- database.js
+++ database.js
@@ -84,6 +84,7 @@ exports.setSensorDescription = function(id, desc, cb){
 
 }
 
+
 exports.setSensorFlag = function(id, flag, cb){
 
 	MongoClient.connect(url, function(err, db) {
@@ -178,6 +179,140 @@ exports.deleteSensor = function(id,cb){
 		console.log("Connected succesfully to database");
 
 		var collection = db.collection('sensors').deleteMany(
+		{'id' : id},
+		function(err, result){
+			console.log("Removed 1 document in the collection");
+
+			if(typeof cb !== 'undefined')
+				cb(result);
+
+		});
+		db.close();
+	});
+}
+
+exports.thirdPartyExists = function(id, cb){
+
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to database");
+
+		var collection = db.collection('thirdParties');
+		// Insert some documents
+		collection.find({'id' : id}.limit(1).toArray(function(err, docs) {
+			assert.equal(err, null);
+			console.log("Inserted 1 document into the collection");
+
+			if(typeof cb !== 'undefined'){
+				if(docs.length == 1)
+					cb(true);
+				else
+					cb(false);
+			}
+		}));
+
+		db.close();
+	});
+
+}
+
+exports.insertThirdParty = function(id, flag, cb){
+
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to database");
+
+		var collection = db.collection('thirdParties');
+		// Insert some documents
+		collection.insertOne({'id' : id, 'flag' : flag},
+		function(err, result) {
+			assert.equal(err, null);
+			assert.equal(1, result.insertedCount);
+			console.log("Inserted 1 document into the collection");
+
+			if(typeof cb !== 'undefined')
+				cb(result);
+		});
+
+		db.close();
+	});
+
+}
+
+exports.setThirdPartyDescription = function(id, desc, cb){
+
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to database");
+
+		var collection = db.collection('thirdParties');
+		// Insert some documents
+		collection.updateOne({'id' : id}, {'description' : desc}, null,
+		function(err, result) {
+			assert.equal(err, null);
+			assert.equal(1, result.result.n);
+			console.log("updated 1 document in the collection");
+
+			if(typeof cb !== 'undefined')
+				cb(result);
+		});
+
+		db.close();
+	});
+
+}
+
+
+exports.setThirdPartyFlag = function(id, flag, cb){
+
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to database");
+
+		var collection = db.collection('thirdParties');
+		// Insert some documents
+		collection.updateOne({'id' : id}, {'flag' : flag}, null,
+		function(err, result) {
+			assert.equal(err, null);
+			assert.equal(1, result.result.n);
+			console.log("updated 1 document in the collection");
+
+			if(typeof cb !== 'undefined')
+				cb(result);
+		});
+
+		db.close();
+	});
+
+}
+
+exports.getThirdPartyFlag = function(id, flag, cb){
+
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to database");
+
+		var collection = db.collection('thirdParties');
+		// Insert some documents
+		collection.find({'id' : id}, {'flag' : flag}.limit(1).toArray(function(err,docs){
+			assert.equal(err, null);
+			console.log("Retrived 1 document in the collection");
+
+			if(typeof cb !== 'undefined')
+				cb(result);
+		}));
+
+		db.close();
+	});
+
+}
+
+exports.deleteThirdParty = function(id,cb){
+	MongoClient.connect(url,function(err,db){
+		assert.equal(null,err);
+		console.log("Connected succesfully to database");
+
+		var collection = db.collection('thirdParties').deleteMany(
 		{'id' : id},
 		function(err, result){
 			console.log("Removed 1 document in the collection");
