351d6f99405a927511b303aa3f953f54f8ea22f5
MrOwlie <fredrik.uvgard@gmail.com>
2017-02-08 15:09:22 +0100

    testing

--- ajax.js
+++ ajax.js
@@ -36,7 +36,7 @@ exports.sensorSubmit = function(chain, db, req, res) {
 				// res.end('{"response":"error"}');
 			// });
 
-			sensorQuery(sensor, chain, "read", [],
+			sensorQuery(sensor, chain, "read", [sensID],
 			function(result){ //completion handler
 
 				db.insertData(/*date*/new Date(), sensID, data);
