1929f8508f7e04cd44e48ee088bec96995bcc4b3
MrOwlie <fredrik.uvgard@gmail.com>
2017-02-20 16:59:59 +0100

    Updated temperature.go The chaincode is now Initialized by sending the admin policy as an argument. Every user needs a policyID argument field. This ID is used as a key in the state database to store the actual policy as a JSON string.

--- chaincode/temperature.go
+++ chaincode/temperature.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
     "encoding/json"
 
+	"github.com/hyperledger/fabric/accesscontrol/impl"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
@@ -19,9 +20,14 @@ type TemperatureChaincode struct {
 //This function will be executed by the chaincode when it is first deployed.
 //I don't think we need any kind of initialization yet. Therefore we take 0 arguments and return nil, nil if that is the case.
 func (t *TemperatureChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 0 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 4")
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1. This argument should be the policy of the admin.")
 	}
+	policyID, err := impl.NewAccessControlShim(stub).ReadCertAttribute("policyID")
+	if err != nil {
+		return nil, errors.New("Could not fetch user policyID. Make sure the policyID attribute is set in the user certificate.")
+	}
+	stub.PutState(policyID, args[0])
 
 	return nil, nil
 }
@@ -29,18 +35,30 @@ func (t *TemperatureChaincode) Init(stub shim.ChaincodeStubInterface, function s
 //Called when someone is trying to perform a transaction to change the state.
 //When adding a policy to allow queries for temperature data, append ("temperature", true) to the policy JSON.
 func (t *TemperatureChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-    caller, err := stub.GetCallerMetadata()
+    policyID, err := impl.NewAccessControlShim(stub).ReadCertAttribute("policyID")
     if err != nil {
-        return nil, errors.New("Could not fetch caller metadata.")
+        return nil, errors.New("Could not fetch user policyID. Make sure the policyID attribute is set in the user certificate.")
     }
-    fmt.Println(caller)
-    if caller == "WebAppAdmin" {
+
+	//Get policy of caller
+	policyString, err := stub.GetState(policyID)
+	if err != nil {
+		return nil, errors.New("Could not fetch user policy from state. Make sure the user has a policy set in the state.")
+	}
+    fmt.Println(policyString)
+	policy map[string]interface{}
+    policy, err := json.Unmarshal(policyString)
+    if policy.admin == true {
         switch function {
+
+			//This case is executed when the recieved function is removePolicy
         case "removePolicy":
+			//The removePolicy function only takes one argument, return an error if there is another amount.
             if len(args) != 1 {
                 return nil, errors.New("Wrong number of arguments for function " + function + ", expected 1 but recieved " + len(args) + ".")
             }
             fmt.Println("Deleting policy for user: " + args[0] + ".")
+			//Delete the policy for user in argument
             return t.delete(stub, args)
         case "addPolicy":
             fmt.Println("Inserting new policy for user: " + arg[0] + ".")
@@ -63,20 +81,22 @@ func (t *TemperatureChaincode) Invoke(stub shim.ChaincodeStubInterface, function
 
 // Query callback representing the query of a chaincode
 func (t *TemperatureChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-    caller, err := stub.GetCallerMetadata()
-
+    //Check arguments
     if len(args != 0) {
         return nil, errors.New("Queries does not take any arguments. Recieved " + len(args) + ".")
     }
-
-    policyString, err := stub.GetState(caller)
+	policyID, err := impl.NewAccessControlShim(stub).ReadCertAttribute("policyID")
+	if err != nil {
+		return nil, errors.New("Could not fetch user policy. Make sure the policy attribute is set in the user certificate.")
+	}
+	policyString, err := stub.GetState(PolicyID)
     policy map[string]interface{}
     policy, err := json.Unmarshal(policyString)
 
     switch function {
     case "temperature":
         if policy[temperature] == true {
-            return []byte(true), errors.New("")
+            return []byte(true), nil
         }
     default:
         return []byte(false), errors.New("Function: " + function + " was not found.")
