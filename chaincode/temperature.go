9fe7fa3ea812205dd8c8f2643340ec601a023c8d
MrOwlie <fredrik.uvgard@gmail.com>
2017-02-20 20:33:22 +0100

    Changed admin verification

--- chaincode/temperature.go
+++ chaincode/temperature.go
@@ -23,11 +23,14 @@ func (t *TemperatureChaincode) Init(stub shim.ChaincodeStubInterface, function s
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting 1. This argument should be the policy of the admin.")
 	}
-	policyID, err := impl.NewAccessControlShim(stub).ReadCertAttribute("policyID")
+	adminCert, err := stub.GetCallerMetadata()
 	if err != nil {
-		return nil, errors.New("Could not fetch user policyID. Make sure the policyID attribute is set in the user certificate.")
+		return nil, errors.New("Could not get caller metadata.")
 	}
-	stub.PutState(policyID, args[0])
+	if len(adminCert) == 0 {
+		return nil, errors.New("Invalid admin certificate, it was empty.")
+	}
+	stub.PutState("admin", adminCert)
 
 	return nil, nil
 }
@@ -35,46 +38,46 @@ func (t *TemperatureChaincode) Init(stub shim.ChaincodeStubInterface, function s
 //Called when someone is trying to perform a transaction to change the state.
 //When adding a policy to allow queries for temperature data, append ("temperature", true) to the policy JSON.
 func (t *TemperatureChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-    policyID, err := impl.NewAccessControlShim(stub).ReadCertAttribute("policyID")
+    adminCert, err := stub.GetCallerMetadata()
     if err != nil {
-        return nil, errors.New("Could not fetch user policyID. Make sure the policyID attribute is set in the user certificate.")
+        return nil, errors.New("Could not get caller metadata.")
     }
 
 	//Get policy of caller
-	policyString, err := stub.GetState(policyID)
+	//policyString, err := stub.GetState(policyID)
+	//if err != nil {
+	//	return nil, errors.New("Could not fetch user policy from state. Make sure the user has a policy set in the state.")
+	//}
+    //fmt.Println(policyString)
+	//policy map[string]interface{}
+    //policy, err := json.Unmarshal(policyString)
+	ok, err := t.isCaller(stub, adminCert)
 	if err != nil {
-		return nil, errors.New("Could not fetch user policy from state. Make sure the user has a policy set in the state.")
+		return nil, errors.New("Failed checking admin certificate.")
 	}
-    fmt.Println(policyString)
-	policy map[string]interface{}
-    policy, err := json.Unmarshal(policyString)
-    if policy.admin == true {
-        switch function {
+	if !ok {
+		return nil, errors.New("The caller is not admin")
+	}
+    switch function {
 
-			//This case is executed when the recieved function is removePolicy
-        case "removePolicy":
-			//The removePolicy function only takes one argument, return an error if there is another amount.
-            if len(args) != 1 {
-                return nil, errors.New("Wrong number of arguments for function " + function + ", expected 1 but recieved " + len(args) + ".")
-            }
-            fmt.Println("Deleting policy for user: " + args[0] + ".")
-			//Delete the policy for user in argument
-            return t.delete(stub, args)
-        case "addPolicy":
-            fmt.Println("Inserting new policy for user: " + arg[0] + ".")
-            err = stub.PutState(arg[0], arg[1])
-            if err != nil {
-                return nil, errors.New("Error occurred when trying to PutState(" + arg[0] + ", " + arg[1] + ").")
-            }
-        default:
-            return nil, errors.New("Function: " + function + " was not found.")
+		//This case is executed when the recieved function is removePolicy
+    case "removePolicy":
+		//The removePolicy function only takes one argument, return an error if there is another amount.
+        if len(args) != 1 {
+            return nil, errors.New("Wrong number of arguments for function " + function + ", expected 1 but recieved " + len(args) + ".")
         }
+        fmt.Println("Deleting policy for user: " + args[0] + ".")
+		//Delete the policy for user in argument
+        return t.delete(stub, args)
+    case "addPolicy":
+        fmt.Println("Inserting new policy for user: " + arg[0] + ".")
+        err = stub.PutState(arg[0], arg[1])
+        if err != nil {
+            return nil, errors.New("Error occurred when trying to PutState(" + arg[0] + ", " + arg[1] + ").")
+        }
+    default:
+        return nil, errors.New("Function: " + function + " was not found.")
     }
-
-    //should a user ever be able to invoke something on the state???
-
-
-
 
 	return nil, nil
 }
